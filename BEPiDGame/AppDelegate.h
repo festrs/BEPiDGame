0717fa54acf53fb0aeb68cce0a6707c2c9609d6a
festrs <felipe.dipereira@gmail.com>
2014-03-27 09:19:43 -0300

    Initial Commit

---
+++ BEPiDGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BEPiDGame
+//
+//  Created by Felipe Dias Pereira on 27/03/14.
+//  Copyright (c) 2014 Felipe Dias Pereira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BEPiDGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BEPiDGame
+//
+//  Created by Felipe Dias Pereira on 27/03/14.
+//  Copyright (c) 2014 Felipe Dias Pereira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
